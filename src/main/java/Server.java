6632c8a3c159a2a3b3c228698770b87865a73af8
Peonsson <xsnudelx@hotmail.com>
2015-12-17 14:59:52 +0100

    fix start server code to correct method

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,13 +2,9 @@
  * Created by Peonsson on 17/12/15.
  */
 
-import io.vertx.core.*;
-import io.vertx.core.http.HttpMethod;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerRequest;
-import io.vertx.core.http.HttpServerResponse;
-import io.vertx.core.net.NetServer;
-import io.vertx.core.net.NetServerOptions;
 
 public class Server extends AbstractVerticle {
     private HttpServer server = null;
@@ -18,31 +14,39 @@ public class Server extends AbstractVerticle {
         // Create server
         server = vertx.createHttpServer();
 
-        server.requestHandler(new Handler<HttpServerRequest>() {
-            @Override
-            public void handle(HttpServerRequest request) {
-                System.out.println("incoming request!");
+        vertx.createHttpServer().requestHandler(request -> {
+            request.response().end("Hello world");
+        }).listen(4000);
 
-                if (request.method() == HttpMethod.GET) {
-                    // Send back a response
-                    HttpServerResponse response = request.response();
-                    response.setStatusCode(200);
-                    response.headers()
-                            .add("Content-Length", String.valueOf(57))
-                            .add("Content-Type", "text/html");
-
-                    response.end("Vert.x is alive!");
-                }
-            }
-        });
-
-        server.listen(4000, res -> {
-            if (res.succeeded()) {
-                System.out.println("Server is now listening!");
-            } else {
-                System.out.println("Failed to bind!");
-            }
-        });
+//        server.requestHandler(new Handler<HttpServerRequest>() {
+//            @Override
+//            public void handle(HttpServerRequest request) {
+//                System.out.println("incoming request!");
+//
+//
+//
+//                if (request.method() == HttpMethod.GET) {
+//                    // Send back a response
+//                    HttpServerResponse response = request.response();
+//
+//                    response.setStatusCode(200);
+//                    response.headers()
+//                            .add("Content-Length", String.valueOf(57))
+//                            .add("Content-Type", "text/html");
+//
+//                    response.write("<html><body>Vert.x is alive!</body></html>");
+//                    response.end();
+//                }
+//            }
+//        });
+//
+//        server.listen(4000, res -> {
+//            if (res.succeeded()) {
+//                System.out.println("Server is now listening!");
+//            } else {
+//                System.out.println("Failed to bind!");
+//            }
+//        });
     }
 
     @Override
