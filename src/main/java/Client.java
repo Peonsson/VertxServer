453d273299bed9458806e7fbfe577c9265427ec3
Peonsson <xsnudelx@hotmail.com>
2015-12-17 17:51:11 +0100

    fix server

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,31 +1,31 @@
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.core.http.HttpClient;
-
-/**
- * Created by Peonsson on 17/12/15.
- */
-public class Client extends AbstractVerticle {
-
-    @Override
-    public void start(Future<Void> startFuture) throws Exception {
-        super.start(startFuture);
-
-        HttpClient client = vertx.createHttpClient();
-
-
-
-        client.websocket(4000, "localhost", "/", websocket -> {
-            websocket.handler(data -> {
-                System.out.println("Received data: " + data.toString("ISO-8859-1"));
-//                client.close();
-            });
-            websocket.writeBinaryMessage(Buffer.buffer("HELLO WRRROLD"));
-            websocket.writeBinaryMessage(Buffer.buffer("HELLO WRRROLD"));
-            websocket.writeBinaryMessage(Buffer.buffer("HELLO WRRROLD"));
-            websocket.writeBinaryMessage(Buffer.buffer("HELLO WRRROLD"));
-
-        });
-    }
-}
+//import io.vertx.core.AbstractVerticle;
+//import io.vertx.core.Future;
+//import io.vertx.core.buffer.Buffer;
+//import io.vertx.core.http.HttpClient;
+//
+///**
+// * Created by Peonsson on 17/12/15.
+// */
+//public class Client extends AbstractVerticle {
+//
+//    @Override
+//    public void start(Future<Void> startFuture) throws Exception {
+//        super.start(startFuture);
+//
+//        HttpClient client = vertx.createHttpClient();
+//
+//
+//
+//        client.websocket(4000, "localhost", "/", websocket -> {
+//            websocket.handler(data -> {
+//                System.out.println("Received data: " + data.toString("ISO-8859-1"));
+////                client.close();
+//            });
+//            websocket.writeBinaryMessage(Buffer.buffer("HELLO WRRROLD"));
+//            websocket.writeBinaryMessage(Buffer.buffer("HELLO WRRROLD"));
+//            websocket.writeBinaryMessage(Buffer.buffer("HELLO WRRROLD"));
+//            websocket.writeBinaryMessage(Buffer.buffer("HELLO WRRROLD"));
+//
+//        });
+//    }
+//}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,6 @@ import io.vertx.core.Vertx;
 public class Main {
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
-//        vertx.deployVerticle(new Client());
+        vertx.deployVerticle(new Server());
     }
 }
