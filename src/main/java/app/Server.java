1c274ac2955cafe77d8e5296f3fd61ac00f339f3
Johan Pettersson <xsnudelx@hotmail.com>
2015-12-18 00:12:02 +0100

    new comments

--- src/main/java/app/Server.java
+++ src/main/java/app/Server.java
@@ -11,10 +11,6 @@ import io.vertx.ext.web.handler.sockjs.BridgeOptions;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 
-import java.text.DateFormat;
-import java.time.Instant;
-import java.util.Date;
-
 public class Server extends AbstractVerticle {
     int counter = 0;
 
@@ -23,32 +19,28 @@ public class Server extends AbstractVerticle {
 
         Router router = Router.router(getVertx());
 
-        // Allow events for the designated addresses in/out of the event bus bridge
+        // define addresses
         BridgeOptions opts = new BridgeOptions()
                 .addInboundPermitted(new PermittedOptions().setAddress("chat.to.server"))
                 .addOutboundPermitted(new PermittedOptions().setAddress("chat.to.client"));
 
-        // Create the event bus bridge and add it to the router.
+        // define path for messages
         SockJSHandler ebHandler = SockJSHandler.create(vertx).bridge(opts);
         router.route("/eventbus/*").handler(ebHandler);
-
-        // Create a router endpoint for the static content.
         router.route().handler(StaticHandler.create());
 
-        // Start the web server and tell it to use the router to handle requests.
+        // listen to port 4000
         vertx.createHttpServer().requestHandler(router::accept).listen(4000);
 
         EventBus eb = vertx.eventBus();
 
-        // Register to listen for messages coming IN to the server
+        // add users to to eventbus
         eb.consumer("chat.to.server").handler(message -> {
 
             System.out.println("message" + counter++ + ": " + message.body().toString());
 
-            // Create a timestamp string
-            String timestamp = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM).format(Date.from(Instant.now()));
-            // Send the message back out to all clients with the timestamp prepended.
-            eb.publish("chat.to.client", timestamp + ": " + message.body().toString());
+            // publish message to all subscribers
+            eb.publish("chat.to.client", message.body().toString());
         });
     }
 }
