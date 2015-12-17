fec4cfb4e1d30480cc33202cbcfdaf3c0565babd
Peonsson <xsnudelx@hotmail.com>
2015-12-17 15:23:10 +0100

    add client

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,6 +1,7 @@
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
 
 /**
  * Created by Peonsson on 17/12/15.
@@ -11,6 +12,16 @@ public class Client extends AbstractVerticle {
     public void start(Future<Void> startFuture) throws Exception {
         super.start(startFuture);
 
-        HttpClient client = vertx.createHttpClient();
+        HttpClientOptions options = new HttpClientOptions().setDefaultPort(4000).setDefaultHost("localhost");
+        HttpClient client = vertx.createHttpClient(options);
+
+        client.getNow(4000, "localhost", "/",response -> {
+
+            response.bodyHandler(totalBuffer -> {
+                // Now all the body has been read
+                System.out.println("Total response body length is " + totalBuffer.length());
+                System.out.println("data is:" + totalBuffer.toString());
+            });
+        });
     }
 }
