9ddca16bc47b9f86eddc5cfc6f09ae80b10956f7
Peonsson <xsnudelx@hotmail.com>
2015-12-17 13:03:42 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Peonsson on 17/12/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server();
+    }
+}
---
+++ src/main/java/Server.java
@@ -0,0 +1,45 @@
+/**
+ * Created by Peonsson on 17/12/15.
+ */
+
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Verticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.net.NetServer;
+
+public class Server implements Verticle {
+
+    public void start()  {
+
+        Vertx vertx = Vertx.vertx();
+        NetServer server = vertx.createNetServer();
+        server.listen(0, "localhost", res -> {
+            if (res.succeeded()) {
+                System.out.println("Server is now listening on actual port: " + server.actualPort());
+            } else {
+                System.out.println("Failed to bind!");
+            }
+        });
+    }
+
+    @Override
+    public Vertx getVertx() {
+        return null;
+    }
+
+    @Override
+    public void init(Vertx vertx, Context context) {
+
+    }
+
+    @Override
+    public void start(Future<Void> future) throws Exception {
+
+    }
+
+    @Override
+    public void stop(Future<Void> future) throws Exception {
+
+    }
+}
