5836d2062f1533c6c1ee10eed207a86178ecf4f0
Peonsson <xsnudelx@hotmail.com>
2015-12-17 13:38:33 +0100

    fix start server code to correct method

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -10,19 +10,6 @@ import io.vertx.core.net.NetServer;
 
 public class Server implements Verticle {
 
-    public void start()  {
-
-        Vertx vertx = Vertx.vertx();
-        NetServer server = vertx.createNetServer();
-        server.listen(0, "localhost", res -> {
-            if (res.succeeded()) {
-                System.out.println("Server is now listening on actual port: " + server.actualPort());
-            } else {
-                System.out.println("Failed to bind!");
-            }
-        });
-    }
-
     @Override
     public Vertx getVertx() {
         return null;
@@ -35,6 +22,16 @@ public class Server implements Verticle {
 
     @Override
     public void start(Future<Void> future) throws Exception {
+
+        Vertx vertx = Vertx.vertx();
+        NetServer server = vertx.createNetServer();
+        server.listen(0, "localhost", res -> {
+            if (res.succeeded()) {
+                System.out.println("Server is now listening on actual port: " + server.actualPort());
+            } else {
+                System.out.println("Failed to bind!");
+            }
+        });
 
     }
 
